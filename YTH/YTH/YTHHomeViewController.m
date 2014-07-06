31eaf1c167b889d2be02296ceec3177ae239e792
Yousra Kamoona <yousraskamoona@gmail.com>
2014-07-06 15:08:21 -0700

    v1

---
+++ YTH/YTH/YTHHomeViewController.m
@@ -0,0 +1,42 @@
+//
+//  YTHHomeViewController.m
+//  YTH
+//
+//  Created by Yousra Kamoona on 7/6/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+#import "YTHHomeViewController.h"
+#import <Parse/Parse.h>
+
+@interface YTHHomeViewController ()
+
+@end
+
+@implementation YTHHomeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    PFObject *testObject = [PFObject objectWithClassName:@"TestObject"];
+    testObject[@"foo"] = @"bar";
+    [testObject saveInBackground];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
